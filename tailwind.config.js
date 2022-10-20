26970e07b7d249fac93eee0751db73b06fda8a51
Jakub Hašek <hasek.kuba@hotmail.cz>
2022-10-20 12:34:16 +0200

    chore: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./*.{html,js,php}"],
   theme: {
+    colors: {
+      'dark': '#2E3532',
+      'yellow': '#FFBF00',
+      'gray': '#C9C5CB',
+      'green': '#648767',
+      'violet': '#7F2CCB'
+    },
     extend: {},
   },
   plugins: [],
