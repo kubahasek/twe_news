1d73ec3f19cdf8bfbca6618f4e7b5be83919b95f
Jakub Hašek <hasek.kuba@hotmail.cz>
2022-10-20 12:10:25 +0200

    fix: tailwind

---
+++ index.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="output.css">
+    <title>The #1 trusted news source!</title>
+</head>
+<body>
+    <h1 class="text-3xl">Testing</h1>
+</body>
+</html>
