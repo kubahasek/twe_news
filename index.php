327ca280c692db5abea9f4312f4cb36137517a7f
Jakub Hašek <hasek.kuba@hotmail.cz>
2022-10-29 19:19:28 +0200

    feat: added index page

--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="output.css">
     <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.3/dist/flowbite.min.css" />
+    <link rel="stylesheet" href="output.css">
     <title>The #1 trusted news source!</title>
 </head>
-<body class="bg-dark">
+<?php 
+        require "utils.php";
+        $articles = getArticles();
+?>
+<body class="bg-dark text-white">
   <!-- colors: https://coolors.co/2e3532-ffbf00-c9c5cb-648767-7f2ccb -->
   <nav class="bg-dark text-gray border-gray-200 px-2 sm:px-4 py-2.5 rounded shadow-xl">
     <div class="container flex flex-wrap justify-between items-center mx-auto">
@@ -20,30 +24,43 @@
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
         <ul class="flex flex-col p-4 mt-4 bg-dark rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
           <li>
-            <a href="#" class="block py-2 pr-4 pl-3 text-dark bg-yellow rounded md:bg-transparent md:text-yellow md:p-0" aria-current="page">Zprávy</a>
+            <a href="/twe_news/" class="block py-2 pr-4 pl-3 text-dark bg-yellow rounded md:bg-dark md:text-yellow md:p-0" aria-current="page">Zprávy</a>
           </li>
           <li>
-            <a href="#" class="block py-2 pr-4 pl-3 text-white hover:text-yellow rounded md:bg-transparent md:p-0 " aria-current="page">Kategorie</a>
+            <a href="/twe_news/category.php" class="block py-2 pr-4 pl-3 text-white hover:text-yellow rounded md:bg-dark md:p-0 " aria-current="page">Kategorie</a>
           </li>
           <li>
-            <a href="#" class="block py-2 pr-4 pl-3 text-white hover:text-yellow rounded md:bg-transparent md:p-0 " aria-current="page">Autoři</a>
+            <a href="#" class="block py-2 pr-4 pl-3 text-white hover:text-yellow rounded md:bg-dark md:p-0 " aria-current="page">Autoři</a>
           </li>
           <li>
-            <a href="#" class="block py-2 pr-4 pl-3 text-white hover:text-yellow rounded md:bg-transparent md:p-0 " aria-current="page">Administrace</a>
+            <a href="#" class="block py-2 pr-4 pl-3 text-white hover:text-yellow rounded md:bg-dark md:p-0 " aria-current="page">Administrace</a>
           </li>
           <li>
-            <a href="#" class="block py-2 pr-4 pl-3 text-white hover:text-yellow rounded md:bg-transparent md:p-0 " aria-current="page">Přidat</a>
+            <a href="#" class="block py-2 pr-4 pl-3 text-white hover:text-yellow rounded md:bg-dark md:p-0 " aria-current="page">Přidat</a>
           </li>
         </ul>
       </div>
     </div>
   </nav>
   <main>
-    <div class="container flex flex-wrap justify-between items-center mx-auto mt-5 md:p-0 px-2 ">
-      <h1 class="text-white text-3xl uppercase font-bold">Články</h1>
-      <div class="mt-5">
-        
-      </div>
+    <div class="container mx-auto mt-5 md:p-0 px-2">
+      <h1 class="text-white text-5xl uppercase font-bold">Články</h1>
+        <p class="text-gray-500">Nejnovější zprávy z IT</p>
+        <div class="mt-5 flex flex-col gap-4 md:m-0 mx-auto w-[90%] md:w-[70%]">
+                <?php foreach($articles as $a): ?>
+                        <article>
+                                <div class="flex gap-2">
+                                        <?php foreach(explode(",", $a["catName"]) as $key => $catName): ?>
+                                                <a class="cursor-pointer text-yellow underline" href="category.php?id=<?= explode(",", $a["catId"])[$key] ?>"><p><?= $catName ?></a>
+                                        <?php endforeach; ?>
+                                </div>
+                                <a href="article.php?id=<?= $a["id"] ?>"><h1 class="text-5xl mb-4 text-yellow hover:cursor-pointer"><?= $a["title"] ?></h1></a>
+                                <p><?= date_format(date_create($a["created_at"]), "d.m.Y H:i") ?> <a class="underline text-yellow" href="author.php?id=<?=$a["authorId"] ?>"><?= $a["authorName"] ?></a></p>
+                                <p><?= $a["perex"] ?></p>
+                                <a href="article.php?id=<?= $a["id"] ?>"><p class="flex underline text-yellow font-bold justify-end hover:cursor-pointer">Číst dál<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="9 18 15 12 9 6"></polyline></svg></p></a>
+                        </article>
+                <?php endforeach; ?>
+        </div>
     </div>
   </main>
     <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
--- utils.php
+++ utils.php
@@ -1,8 +1,20 @@
 <?php
 
-require "db.php";
-
 function run($sql, $data = []){
-    $stmt = $conn -> prepare($sql);
-    $stmt -> execute($data)
+        require "db.php";
+        $stmt = $conn -> prepare($sql);
+        $stmt -> execute($data);
+        return $stmt -> fetchAll();
+}
+
+function getArticles(): array {
+        $sql = "
+        SELECT ar.*, concat(a.name, ' ', a.surname) as authorName, GROUP_CONCAT(c.name) as catName, GROUP_CONCAT(c.id) as catId, a.id as authorId FROM article ar
+        INNER JOIN author a on ar.author_id = a.id
+        INNER JOIN article_category ac on ar.id = ac.article_id
+        INNER JOIN category c on ac.category_id = c.id
+        GROUP BY ar.id
+        LIMIT 5
+        ";
+        return run($sql);
 }
